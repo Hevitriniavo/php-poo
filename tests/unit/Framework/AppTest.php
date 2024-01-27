90888af48e432aad3c2952f9f33c7a4ff87070bb
Hevitriniavo <hei.tantely@gmail.com>
2024-01-27 14:21:12 +0300

    fix added test router 2

--- tests/unit/Framework/AppTest.php
+++ tests/unit/Framework/AppTest.php
@@ -2,9 +2,13 @@
 
 namespace unit\Framework;
 
+use App\Blog\BlogModule;
 use Framework\App;
 use GuzzleHttp\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Tests\Framework\Modules\ErroredModule;
+use Tests\Framework\Modules\StringModule;
 
 class AppTest extends TestCase {
     public function testRedirectTrainingSlash(){
@@ -17,13 +21,47 @@ class AppTest extends TestCase {
     }
 
     public function testBlog(){
-        $app = new App();
+        $app = new App([
+            BlogModule::class
+        ]);
         $request = new ServerRequest("GET", "/blog");
         $response = $app->run($request);
 
-        $this->assertStringContainsString('<h1>Bienvenue sur le blog</h1>', (string) $response->getBody());
         $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('<h1>Bienvenue sur le blog</h1>', (string) $response->getBody());
+
+
+        $requestSingle = new ServerRequest("GET", "/blog/article-de-test");
+        $responseSingle = $app->run($requestSingle);
+        $this->assertStringContainsString('<h1>Bienvenue sur l\'article article-de-test</h1>', (string) $responseSingle->getBody());
+
     }
+
+
+    public function testThrowExceptionIfNoResponseSent()
+    {
+        $app = new App([
+            ErroredModule::class
+        ]);
+
+        $request = new ServerRequest('GET', '/demo');
+        $this->expectException(\Exception::class);
+        $app->run($request);
+    }
+
+    public function testConvertStringToResponse()
+    {
+        $app = new App([
+            StringModule::class
+        ]);
+
+        $request = new ServerRequest('GET', '/demo');
+        $response = $app->run($request);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals("DEMO", (string) $response->getBody());
+    }
+
 
     public function testError404(){
         $app = new App();
